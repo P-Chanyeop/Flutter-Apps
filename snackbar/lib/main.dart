e244e08256fda9d846cec5698fb68e73681f4eea
P-Chanyeop <qkrcksduq1gh@naver.com>
2023-03-20 00:02:42 +0900

    add Toast message widget

--- snackbar/lib/main.dart
+++ snackbar/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,12 +7,12 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title: "snackbar app",
+      title: "snackbar/toast app",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.red
       ),
-      home: MyPage(),
+      home: ToastPage(),
     );
   }
 }
@@ -51,4 +52,69 @@ class MyPage extends StatelessWidget {
   }
 }
 
+class MyPage2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Snack Bar'),
+        centerTitle: true,
+      ),
+      body: MySnackBar(),
+    );
+  }
+}
 
+class MySnackBar extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:
+            Text('Hello',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white
+              ),
+            ),
+            backgroundColor: Colors.cyan,
+            duration: Duration(milliseconds: 1000),
+          ),
+          );
+        },
+        child: Text('Show me'),
+      ),
+    );
+  }
+}
+
+class ToastPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Toast Message'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: (){
+            flutterToast();
+          },
+          child: Text('Toast'),
+        ),
+      ),
+    );
+  }
+}
+
+void flutterToast(){
+  Fluttertoast.showToast(msg: 'Flutter!',
+    gravity: ToastGravity.BOTTOM,
+    backgroundColor: Colors.redAccent,
+    fontSize: 20.0,
+    textColor: Colors.white,
+    toastLength: Toast.LENGTH_SHORT
+  );
+}
